9e0e11addf94b8b7c35b784d388d480174dd692e
xiahrulz-eng <xiahrulz@gmail.com>
2026-07-13 17:32:52 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+/* 수지FC 라인업 생성기 - 서비스워커 (오프라인 지원) */
+const CACHE = 'suji-fc-v9';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './xlsx.full.min.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+
+  // 페이지 요청: 네트워크 우선(최신 유지), 오프라인이면 캐시
+  if (req.mode === 'navigate') {
+    e.respondWith(
+      fetch(req).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put('./index.html', clone));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  // 그 외 자원: 캐시 우선, 없으면 네트워크(동일 출처는 런타임 캐싱)
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res => {
+      try {
+        if (new URL(req.url).origin === location.origin) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(req, clone));
+        }
+      } catch (_) {}
+      return res;
+    }).catch(() => hit))
+  );
+});
